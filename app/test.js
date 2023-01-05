f4e42dadb78b98714d6921cec4337997bb8ad285
lixutao <179665943@qq.com>
2023-01-05 15:09:17 +0800

    clone commit

--- app/test.js
+++ app/test.js
@@ -1,6 +1,5 @@
 var  arr = [1, 2, 3, 4, 5];
 sum = arr.reduce(function(prev, cur, arr) {
-     //输出的是第一项的值或上一次叠加的结果，正在被处理的元素，正在被处理的元素的索引值
      console.log(prev, cur);  
      return prev + cur;
 })
